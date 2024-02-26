669217b5736e7bd5e14862334ed4422e3d68659f
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2024-02-26 21:41:58 +0500

    update

--- src/app/board/[boardId]/actions.ts
+++ src/app/board/[boardId]/actions.ts
@@ -1,36 +1,54 @@
 'use server';
-
-import { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
 import { kv } from "@vercel/kv";
 import { revalidatePath } from "next/cache";
-import { RowElements } from "./utils";
+// import { RowElements } from "./utils";
+import _ from "lodash";
+import { Elements } from "./board";
+import { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
 
+export interface RowElements {
+  [id: string]: string;
+}
 
 
 export const getBoardElements = async (boardId: string) => {
   'use server';
   try {
-    const els = await kv.hgetall(`board:${boardId}`);
-    revalidatePath(`board/${boardId}`);
-    return els as RowElements;
+    const els = await kv.hgetall<Record<string, unknown>>(`board:${boardId}`) || {};
+    // revalidatePath(`board/${boardId}`);
+    console.log('get els', typeof els);
+
+    const elements: Elements = {};
+    Object.keys(els).forEach(el => {
+      console.log('el', el);
+      console.log('el val', typeof els[el]);
+      elements[el] = els[el] as ExcalidrawElement;
+    });
+    console.log('getBoard', elements);
+    return elements;
   } catch (e) {
     console.log(e);
   }
   return {};
 };
 
-
-export const setBoardElement = async (boardId: string, els: RowElements) => {
+export const setBoardElement = _.throttle(async (boardId: string, els: Elements) => {
   'use server';
   try {
+    const elements: RowElements = {};
+    Object.keys(els).forEach(el => {
+      elements[el] = JSON.stringify(els[el]);
+    });
+    // return elements;
+
     console.log('calling it');
-    const idx = await kv.hset(`board:${boardId}`, { ...els });
-    revalidatePath(`board/${boardId}`);
+    const idx = await kv.hset(`board:${boardId}`, { ...elements });
+    // revalidatePath(`board/${boardId}`);
     return idx;
   } catch (e) {
     console.log(e);
   }
-};
+}, 200);
 
 //TODO: delete element
 export const deleteBoardElement = async (boardId: string, elId: string) => {
--- src/app/board/[boardId]/board.tsx
+++ src/app/board/[boardId]/board.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import React, { useCallback, useEffect, useState } from 'react';
 
 import { Excalidraw, LiveCollaborationTrigger } from "@excalidraw/excalidraw";
@@ -9,8 +10,35 @@ import useLocalStorageState from 'use-local-storage-state';
 import { useDataStore } from '@/lib/useDataStore';
 import useStore from '@/lib/useStore';
 import { getBoardElements, setBoardElement } from './actions';
-import { Elements, RowElements, parseElements, stringifyElements } from './utils';
+// import { Elements, RowElements, parseElements, stringifyElements } from './utils';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { getSceneVersion } from "@excalidraw/excalidraw";
+
+// import type { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
+
+export interface RowElements {
+  [id: string]: string;
+}
+
+export interface Elements {
+  [id: string]: ExcalidrawElement;
+}
+export const parseElements = (els: RowElements) => {
+  const elements: Elements = {};
+  for (const el in els) {
+    elements[el] = JSON.parse(els[el]);
+  }
+  console.log('elements parsr', elements);
+  return elements;
+};
+
+export const stringifyElements = (els: Elements) => {
+  const elements: RowElements = {};
+  for (const el in els) {
+    elements[el] = JSON.stringify(els[el]);
+  }
+  return elements;
+};
 
 
 export default function Board(
@@ -21,51 +49,73 @@ export default function Board(
 ) {
   const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI>();
   const [theme, setTheme] = useLocalStorageState('theme');
-  const [locEls, setLocEls] = useLocalStorageState<Elements>(
-    `elements:${boardId}`, { defaultValue: {} }
-  );
+  // const [locEls, setLocEls] = useLocalStorageState<Elements>(
+  //   `elements:${boardId}`, { defaultValue: {} }
+  // );
 
   const queryClient = useQueryClient();
 
   const query = useQuery({
-    queryKey: ['elements'], queryFn: async () => {
+    queryKey: ['elements'],
+    queryFn: async () => {
       const els = await getBoardElements(boardId);
-      setLocEls(parseElements(els));
-      return parseElements(els);
+      console.log('theels2', els);
+      // setLocEls(parseElements(els));
+
+      // console.log('elements parsr', elements);
+      return els;
+      // console.log('parse', await parseElements( els));
+      // return elements;
     }
   });
 
   const mutation = useMutation({
     mutationFn: async (elementsToUpdate: Elements) => {
-      await setBoardElement(boardId, stringifyElements(elementsToUpdate));
-      setLocEls(((prev) => ({ ...prev, ...elementsToUpdate })));
+      await setBoardElement(boardId, elementsToUpdate);
     },
-    onSuccess: () => {
+    onMutate: async (elementsToUpdate: Elements) => {
+      await queryClient.cancelQueries({ queryKey: ['todos'] });
+      queryClient.setQueryData(['elements'], (old: Elements) => ({ ...old, elementsToUpdate }));
+      return elementsToUpdate;
+    },
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ['elements'] });
     },
   });
+  const [version, setVersion] = useState();
+  useEffect(() => {
+    excalidrawAPI?.onChange(_.throttle(async (els: readonly ExcalidrawElement[], appState: AppState) => {
+      const ver = getSceneVersion(els);
+      console.log(ver);
 
-  excalidrawAPI?.onChange(_.throttle(async (els: readonly ExcalidrawElement[], appState: AppState) => {
-    const elementsToUpdate: Elements = {};
-    for (const el of els) {
-      let localEl = query.data?.[el.id];
+      if (version === ver) return;
+      setVersion(version);
 
-      console.log('idx', localEl?.id);
-      console.log('updated:el', el.updated);
-      if (!localEl || el.updated !== localEl.updated) {
-        elementsToUpdate[el.id] = el;
+      const elementsToUpdate: Elements = {};
+      els.forEach((el) => {
+        let localEl = query.data?.[el.id];
+
+        console.log('idx', localEl?.id);
+        console.log('updated:el', el.updated);
+        if (!localEl || el.updated !== localEl.updated) {
+          elementsToUpdate[el.id] = el;
+        }
+      });
+      if (!_.isEmpty(elementsToUpdate)) {
+        mutation.mutate(elementsToUpdate);
+        // setLocStorageElements((prev) => ({ ...prev, ...elementsToUpdate }));
       }
-    }
-    if (!_.isEmpty(elementsToUpdate)) {
-      mutation.mutate(elementsToUpdate);
-      // setLocStorageElements((prev) => ({ ...prev, ...elementsToUpdate }));
-    }
-    if (appState.theme !== theme) {
-      setTheme(appState.theme);
-    }
+      if (appState.theme !== theme) {
+        setTheme(appState.theme);
+      }
 
-  }, 400));
+    }, 1000));
 
+  }, [excalidrawAPI]);
+
+  if (query.isLoading) return;
+
+  console.log('the data', query.data);
 
   return (
     <div className="h-screen">
