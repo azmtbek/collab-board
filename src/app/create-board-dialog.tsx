18c4ac63f69eb31eef90c31c86b5a50e2358b022
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2024-02-26 22:23:02 +0500

    update user session

--- src/app/create-board-dialog.tsx
+++ src/app/create-board-dialog.tsx
@@ -12,11 +12,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { PlusIcon } from '@radix-ui/react-icons';
-import { kv } from '@vercel/kv';
 import { useState } from "react";
-import useLocalStorageState from "use-local-storage-state";
-import { nanoid } from 'nanoid';
-import { redirect, useRouter } from "next/navigation";
 
 
 interface Props {
@@ -25,24 +21,17 @@ interface Props {
 
 export default function CreateBoardDialog({ createBoard }: Props) {
   const [name, setName] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  // const router = useRouter();
-  const [nickname, setNickname] = useLocalStorageState('nickname', { defaultValue: 'doe' });
 
-  // const createBoard = () => { };
-
-  // const boards = await kv.lrange<string>('boards', 0, -1);
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button className="w-64 text-lg py-4 gap-1" ><PlusIcon /> create new board</Button>
-        {/* <Button variant="outline">Edit Profile</Button> */}
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Create a board</DialogTitle>
           <DialogDescription>
-            Please name your board. Choose a nickname for yourself to enter.
+            Please name your board.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
@@ -51,12 +40,6 @@ export default function CreateBoardDialog({ createBoard }: Props) {
               Board Name
             </Label>
             <Input id="name" value={name} onChange={(e) => setName(e.target.value)} className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="nickname" className="text-right">
-              Nickname (used to access)
-            </Label>
-            <Input id="nickname" value={nickname} onChange={(e) => setNickname(e.target.value)} className="col-span-3" />
           </div>
         </div>
         <DialogFooter>
